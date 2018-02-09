2dfd5d6d246887ae36bc6a2f9e7df2cf01cec5a1
Jirat Kijlerdpornpailoj <saakyz@gmail.com>
2018-02-10 00:38:39 +0700

    connect apollo in Post page

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -3,17 +3,68 @@ import PostContent from '../components/PostContent'
 import Comment from '../components/Comment'
 import CommentBox from '../components/CommentBox'
 
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+
 class Post extends React.Component {
   render() {
+    const { loading, post } = this.props
+    if (loading) {
+      return <p>Loading</p>
+    }
+    console.log(this.props)
     return (
       <div>
-        <PostContent />
-        <Comment />
-        <Comment />
+        <PostContent post={post}/>
+        {
+          post.comments.map(comment => <Comment comment={comment} />)
+        }
         <CommentBox />
       </div>
     )
   }
 }
 
-export default Post
+const query = gql`
+query post($postId: Int!) {
+  post(id: $postId) {
+    id
+    title
+    content
+    user {
+      id
+      username
+    }
+    
+    comments {
+      id
+      content
+      user {
+        id
+        username
+      }
+    }
+  }
+}
+`
+
+export default graphql(query, {
+  options: (props) => {
+    const { match } = props
+    const { params } = match
+    const { id } = params
+    console.log(id)
+    return {
+      variables: {
+        postId: id
+      }
+    }
+  },
+  props: ({ data }) => {
+    console.log(data)
+    return {
+      post: data.post,
+      loading: data.loading
+    }
+  } 
+})(Post)
