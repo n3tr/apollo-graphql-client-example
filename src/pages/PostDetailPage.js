b9d01d23b3c66435d394fe74d49394b445e301bd
Jirat Kijlerdpornpailoj <saakyz@gmail.com>
2018-02-11 02:03:02 +0700

    Hide commnet box if token not exists

--- src/pages/PostDetailPage.js
+++ src/pages/PostDetailPage.js
@@ -2,10 +2,11 @@ import React from 'react'
 import PostContent from '../components/PostContent'
 import Comment from '../components/Comment'
 import CommentBox from '../components/CommentBox'
+import { connect } from 'react-redux'
 
 class Post extends React.Component {
   state = {
-    comment: '',
+    comment: ''
   }
 
   onCommentChange = value => {
@@ -22,14 +23,21 @@ class Post extends React.Component {
         <PostContent />
         <Comment />
         <Comment />
-        <CommentBox
-          value={this.state.comment}
-          onChange={this.onCommentChange}
-          onSubmit={this.onSubmitComment}
-        />
+        {this.props.isLoggedIn ? (
+          <CommentBox
+            value={this.state.comment}
+            onChange={this.onCommentChange}
+            onSubmit={this.onSubmitComment}
+          />
+        ) : null}
       </div>
     )
   }
 }
 
-export default Post
+export default connect((state, ownProps) => {
+  return {
+    ...ownProps,
+    isLoggedIn: !!state.token
+  }
+})(Post)
